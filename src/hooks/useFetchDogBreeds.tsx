ae224690552547dc7e2ab8b818f7046e90d08c3b
bskyn <bao.cnguyen2@gmail.com>
2025-01-16 17:55:05 -0800

    feat: add shadcn components for forms and inputs, and breeds fetch

---
+++ src/hooks/useFetchDogBreeds.tsx
@@ -0,0 +1,17 @@
+import { DOG_BREEDS_URL } from '@/constants/api.constants';
+import { apiClient } from '@/utils/api.client';
+import { useQuery } from '@tanstack/react-query';
+
+const fetchDogBreeds = async () => {
+  const { data } = await apiClient.get(DOG_BREEDS_URL);
+  return data;
+};
+
+const useFetchDogBreeds = () => {
+  return useQuery({
+    queryKey: ['dog_breeds'],
+    queryFn: () => fetchDogBreeds(),
+  });
+};
+
+export default useFetchDogBreeds;
